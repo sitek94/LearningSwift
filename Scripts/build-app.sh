aa9c2b1273f1d5e453b4c0dfca8a8ca2e229967e
Maciek Sitkowski <msitkowski94@gmail.com>
2025-06-02 02:11:31 +0200

    archive and export build

--- Scripts/build-app.sh
+++ Scripts/build-app.sh
@@ -5,11 +5,19 @@ set -ex
 version="$(cat $VERSION_FILE)"
 build_number="$GITHUB_RUN_NUMBER"
 
+# Create archive
 xcodebuild -project LearningSwift.xcodeproj -scheme LearningSwift \
   -configuration Release \
-  -derivedDataPath build \
+  -archivePath build/LearningSwift.xcarchive \
+  archive \
   MARKETING_VERSION="$version" \
   CURRENT_PROJECT_VERSION="$build_number"
+
+# Export the archive
+xcodebuild -exportArchive \
+  -archivePath build/LearningSwift.xcarchive \
+  -exportPath "$BUILD_DIR/$XCODE_BUILD_PATH" \
+  -exportOptionsPlist ExportOptions.plist
 
 # Verify the build
 file "$BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
